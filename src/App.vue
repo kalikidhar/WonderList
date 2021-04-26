1a557b5ad9a148e367e6e6608883b8189fc9e2d6
kalikidhar <kalikidhar@gmail.com>
2021-04-27 01:59:37 +0530

    Added Basic Todo Functionalities

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+<div id="app" class="container">
+
+  <img alt="Vue logo" src="./assets/logo1.png">
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <todo-list></todo-list>
+</div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import TodoList from './components/TodoList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TodoList
+  }
+}
+</script>
+
+<style>
+
+* {
+  box-sizing: border-box;
+
+}
+
+.container {
+  max-width: 600px;
+  margin: 0 auto; 
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size:24px;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,142 @@
+<template>
+  <div>
+      <i>Enter your Tasks here !!</i>
+      <input type="text" class="todo-input" placeholder="What task is on your mind ??"
+      v-model="newTodo" @keyup.enter="addTodo">
+    
+  <div v-for="(todo,index) in todos" :key="todo.id" class="todo-item" >
+      <div class="todo-item-left">
+          <div v-if="!todo.editing" @dblclick="editTodo(todo)" class="todo-item-label">
+      {{todo.title}}
+      
+     </div>
+      <input v-else class="todo-item-edit" type="text" v-model="todo.title"
+      @blur="doneEdit(todo)" @keyup.enter="doneEdit(todo)" >
+
+      </div>
+      <div class='remove-item' @click="removeTodo(index)">
+          &times;
+      </div>
+  </div>
+  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'todo-list',
+  props: {
+    msg: String
+  },
+  data() {
+      return {
+          newTodo: "",
+          idForTodo: 3,
+          todos: [
+              {
+                'id': 1,
+                'title': 'Finish Lab',
+                'completed':false, 
+                'editing':false 
+              },
+              {
+                'id': 2,
+                'title': 'Play FIFA',
+                'completed':false, 
+                'editing':false 
+              },
+
+          ]
+      }
+  },
+
+
+  methods:
+  {
+      addTodo()
+      {
+          if(this.newTodo.trim().length==0)
+          {
+              return
+          }
+          this.todos.push({
+              id:this.idForTodo,
+              title:this.newTodo,
+              completed:false,
+          })
+          this.newTodo='',
+          this.idForTodo++
+      },
+
+      removeTodo(index)
+      {
+         this.todos.splice(index,1)
+      },
+
+      editTodo(todo)
+      {
+          todo.editing=true;
+
+      },
+
+      doneEdit(todo)
+      {
+          todo.editing=false;
+      }
+  }
+
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style >
+.todo-input {
+    width: 500px;
+    padding:10px 18px;
+    font-size:18px;
+    margin-bottom: 25px;
+}
+
+.todo-item{
+    margin-bottom:12px;
+    display:flex;
+    align-items:center;
+    justify-content:space-between;
+}
+
+.remove-item {
+
+    cursor: pointer;
+    margin-left: 14px;
+    /* &:hover {
+       color:black;
+    } */
+
+} 
+
+.todo-item-left{
+    display: flex;
+    align-items: center;
+}
+
+.todo-item-label{
+    padding:10px;
+    border: 1px solid white;
+    margin-left:12px;
+}
+
+.todo-item-edit{
+    font-size: 24px;
+    color: #062e5b;
+    margin-left:12px;
+    width:100%;
+    padding:10px;
+    border:1px solid #ccc;
+    font-family:'Avenir',Helvetica,Arial,sans-serif;
+
+   
+}
+
+
+</style>
